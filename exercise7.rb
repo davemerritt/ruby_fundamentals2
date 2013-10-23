9a14aced4d5c43d328bd8b14b773c1f9971e0c4e
davemerritt <davidmerritt6@gmail.com>
2013-10-23 19:30:50 -0400

    Exercise 7 complete

--- exercise7.rb
+++ exercise7.rb
@@ -3,3 +3,29 @@ students = {
 	:cohort2 => 42,
 	:cohort3 => 22
 }
+
+def cohort(size)
+	size.each { |a, b| puts "#{a}: #{b} students"}
+end
+
+students[:cohort4] = "43"
+cohort(students)
+
+puts " "
+
+puts students.keys 
+
+def increase(value)
+	value.each { |a, b| puts "#{a}: #{b * 1.05} students" }
+end
+
+puts " "
+
+increase(students)
+
+puts " "
+
+students.delete(:cohort2)
+cohort(students)
+
+#All but the bonus. Break time! 
